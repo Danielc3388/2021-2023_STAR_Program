31c7f07c695d1fd3decc489cb5039d6e171371b4
Danielc3388 <102593818+Danielc3388@users.noreply.github.com>
2023-05-29 15:05:01 +0800

    source code

--- May 29/Cartonize.py
+++ May 29/Cartonize.py
@@ -13,7 +13,16 @@ def cartoonizeEffect(img_rgb):
     for i in range(2):
         img_color = cv2.pyrUp(img_color)
 
-    return img_color
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
+    img_blur = cv2.medianBlur(img_gray, 3)
+
+    img_edge = cv2.adaptiveThreshold(img_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 2)
+
+    (x,y,z) = img_color.shape
+    img_edge = cv2.resize(img_edge, (y,x))
+    img_edge = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2RGB)
+
+    return cv2.bitwise_and(img_color, img_edge)
 
 file_name = "Picture.jpg"
 result = cartoonizeEffect(file_name)
